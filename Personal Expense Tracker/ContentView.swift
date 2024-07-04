774574063daa8e4037d3511189b1fc7f01e60737
AshmitGupta <67188586+AshmitGupta@users.noreply.github.com>
2024-07-03 22:04:34 -0700

    Initial Commit

---
+++ Personal Expense Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Personal Expense Tracker
+//
+//  Created by Ashmit Gupta on 2024-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Personal Expense Tracker/Personal_Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Expense_TrackerApp.swift
+//  Personal Expense Tracker
+//
+//  Created by Ashmit Gupta on 2024-07-03.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
